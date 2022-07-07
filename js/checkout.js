48a7914ceba5a9b5c3d1cc3d74ca230d9f684cf0
Elihu Romero <elihuromero@outlook.com>
2022-07-07 16:36:37 -0500

    ajax

--- js/checkout.js
+++ js/checkout.js
@@ -8,4 +8,36 @@ function validar(e)
     var formData = new FormData(form);
     //formData.append("nombre",document.getElementById("nombre").value);
     //console.log(formData);
+
+    $.ajax({
+        url: "guardaReserva.php",  
+        type: 'POST',
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        dataType: 'json',
+        //mientras enviamos el archivo
+        beforeSend: function()
+        {
+            document.getElementById("enviar").html("<img src='../img/ajax-loader.gif' width='32' height='32' />");        
+        },
+        //una vez finalizado correctamente
+        success: function(data)
+        {
+            if(data.success)
+            {
+                
+            }
+            else
+            {
+                
+            }
+        },
+        //si ha ocurrido un error
+        error: function()
+        {
+            document.getElementById("enviar").html('<input type="submit" name="guardar" id="guardar" value="Guardar">');
+        }
+    });
 }
